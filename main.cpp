66afabfb966433df85659aab07b9441b0ca35803
Murali Dhanakoti <dhanakoti.murali@gmail.com>
2021-12-13 15:42:16 -0800

    Added command line argument support to load command file at startup. Removed redundant class driver

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
+#include <fstream>
+#include "CommandProcessor.h"
+#include "Command.h"
+#include <vector>
 using namespace std;
 
-#include "Driver.h"
-#include <fstream>
-
 int main(int argc, char** argv) {
-    Driver d;
+    // Create command file
     ofstream ofs("commands.txt");
     ofs << "load <Candidates File>\nlist\ntally\nresults <Candidate Name>\n";
     ofs.close();
-    d.loadCommands("commands.txt");
+    CommandProcessor& cmdProc = CommandProcessor::getInstance();
+    cmdProc.initialize("commands.txt");
+    // Load candidates file if the user provided it
+    if (argc == 2) {
+        LoadCandidatesCommand().execute({argv[1]});
+    }
+    cmdProc.cmdLoop();
     return 0;
 }
